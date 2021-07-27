6deaf71bd23867ad191323b57745077020a60a9f
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-27 17:33:20 +0900

    [#1] style : 추천페이지 버튼 뷰 완성
    활성화 버튼, 비활성화 버튼 뷰

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Main from "./pages/Main";
+import Recommand from "./pages/Recommand";
 
 const App = (props) => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/recommand" component={Recommand} />
       </Switch>
     </Router>
   );
