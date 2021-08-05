51103cf00ecb24588c3f106caea922ca7a2fb4b1
1117JHH <74212632+janghwahyun@users.noreply.github.com>
2021-08-05 22:00:59 +0900

    [#24] style : 360 x 640 뷰 제작

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import DelightLogo from "../image/DelightLogo.png";
 import Google from "../image/googlelogo.png";
 import Kakao from "../image/kakaologo.png";
-import Facebook from "../image/facebooklogo.png";
+import Naver from "../image/naverlogo.png";
 
 import { Grid, Text } from "../elements";
 
@@ -13,20 +13,16 @@ import { GOOGLE_URL, KAKAO_URL, NAVER_URL } from '../redux/modules/oauth';
 
 
 const Login = () => {
-
-
-
-
     return (
         <React.Fragment>
-            <Grid is_flex2 wrap width="100%" margin="150px 0 0 0">
-                <Grid is_flex2 margin="0 0 50px 0">
+            <Grid is_flex2 wrap width="100%" height="10%" margin="10rem 0 0 0">
+                <Grid is_flex2 margin="0 0 5rem 0">
                     <img src={DelightLogo} />
                 </Grid>
 
-                <Grid is_flex2 margin="0 0 20px 0">
-                    <Text size="16px" bold>
-                        당신에게 만족스러운<br />한끼의 기쁨을 드려요
+                <Grid is_flex2 margin="0 0 1rem 0">
+                    <Text size="1.6rem" bold>
+                        당신에게 만족스러운<br /><br />한끼의 기쁨을 드려요
                 </Text>
                 </Grid>
 
@@ -48,7 +44,7 @@ const Login = () => {
 
                     <ButtonN href={NAVER_URL}>
                         <Grid is_flex2>
-                            <LogoF />
+                            <LogoN />
                             <Text color="white" size="16px" bold text_align="center" margin="auto">네이버 1초 로그인</Text>
                         </Grid>
                     </ButtonN>
@@ -63,12 +59,12 @@ const ButtonG = styled.a`
     background-color: #E7E7E7;
     border:none;
     color:#ffffff;
-    border-radius:30px;
-    width:500px;
-    height:60px;
+    border-radius:3rem;
+    width:50rem;
+    height:6rem;
     cursor:pointer;
-    padding:0 10px 0 50px;
-    margin:10px 0 10px 0;
+    padding:0 1rem 0 5rem;
+    margin:1rem 0 1rem 0;
     text-decoration: none;
 `;
 const ButtonK = styled.a`
@@ -84,7 +80,7 @@ const ButtonK = styled.a`
     text-decoration: none;
 `;
 const ButtonN = styled.a`
-    background-color: #4068F4;
+    background-color: #03C75A;
     border:none;
     color:#ffffff;
     border-radius:30px;
@@ -110,11 +106,11 @@ height: 30px;
 background-size: cover;
 background-image: url(${Kakao});
 `;
-const LogoF = styled.div`
+const LogoN = styled.div`
 width: 30px;
 height: 30px;
 background-size: cover;
-background-image: url(${Facebook});
+background-image: url(${Naver});
 `;
 
 
