e586029c62c79449a4e6a69836a49e83593fe95f
HyunJung <57293780+yanghyunjung@users.noreply.github.com>
2021-08-25 18:32:10 +0900

    [#118] feat : 반응형 초기 세팅

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import "./index.css";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/shared/theme";
 
 ReactDOM.render(
   <Provider store={store}>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root"));
 
